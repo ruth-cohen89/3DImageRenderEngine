c1fe54f17390358cedc023e4c5e73977a450ae9c
Ruth <ruthcohe@g.jct.ac.il>
2021-04-11 02:28:24 +0300

    2:27 11/4

--- tests/geometries/TubeTest.java
+++ tests/geometries/TubeTest.java
@@ -1,12 +1,30 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TubeTest {
 
+    //test if the function returns a correct normal
     @Test
     void testGetNormal() {
+
+        Tube tube = new Tube(new Ray(new Point3D(0, 0, 1), new Vector(0, 0, 1)), 1);
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Simple case of generate normal
+        Vector v1 = new Vector(0, 1, 0);
+        Vector v2 = tube.getNormal(new Point3D(0, 1, 2));
+        assertEquals(v1, v2, "ERROR: wrong normal");
+
+        // =============== Boundary Values Tests ==================
+
+        // TC10: The point is in front of the head of the ray
+        Vector v3 = new Vector(0, 1, 0);
+        Vector v4 = tube.getNormal(new Point3D(0, 1, 1));
+        assertEquals(v3, v4, "ERROR: wrong normal");
     }
 }//f
