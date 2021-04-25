470c884fab5440e47f082008507be78da2e0d520
Ruth <ruthcohe@g.jct.ac.il>
2021-04-25 13:03:04 +0300

    13:02 25/04

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -7,7 +7,7 @@ import primitives.Vector;
 import static primitives.Util.isZero;
 
 public class Camera {
-    private final Point3D _p0;
+    private final Point3D _P0;
     private final Vector _vTo;
     private final Vector _vUp;
     private final Vector _vRight;
@@ -17,94 +17,118 @@ public class Camera {
     private double _width;
     private double _distance;
 
-    public Camera(Point3D p0, Vector vTo, Vector vUp) {
-        _p0=p0;
-        _vTo=vTo.normalized();
-        _vUp=vUp.normalized();
-        _vRight=_vTo.crossProduct(_vUp);
+    private Camera(Builder Builder) {
+        _P0 = Builder._P0;
+        _vTo = Builder._vTo;
+        _vUp = Builder._vUp;
+        _vRight = Builder._vRight;
+        _height = Builder._height;
+        _width = Builder._width;
+        _distance = Builder._distance;
     }
 
-    /**return the value of the point*/
-    public Point3D getP0() {
-        return _p0;
-    }
-
-    /**return the value of the vector who goes up*/
-    public Vector getvUp() {
-        return _vUp;
-    }
-
-    /**return the value of the vector who goes forward*/
-    public Vector getvTo() {
-        return _vTo;
-    }
-
-    /**return the value of the vector who goes right*/
-    public Vector getvRight() {
-        return _vRight;
-    }
-
-    /**setter for the height and width
-     * return the camera*/
+    /**
+     * set the length and width of ViewPlane
+     * @param width  width of the ViewPlane
+     * @param height height of the ViewPlane
+     * @return this
+     */
     public Camera setViewPlaneSize(double width, double height) {
-        _height=height;
-        _width=width;
+        _width = width;
+        _height = height;
         return this;
     }
-
-    /**set the distance for the view plane*/
+    /**
+     * set the distance of ViewPlane
+     * @param distance The distance between the View Plane to the camera
+     * @return this
+     */
     public Camera setDistance(double distance) {
-        _distance=distance;
+        _distance = distance;
         return this;
     }
 
-    /**intersections of ray with pixels im the view plane*/
+    /**
+     * construct a Ray through Pixel
+     * @param nX     number of pixels on x axis
+     * @param nY     number of pixels on y axis
+     * @param j     index of the pixel on the view plane on the x axis
+     * @param i     index of the pixel on the view plane on the y axis
+     * @return       the Ray through Pixel
+     */
     public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
-        Point3D Pc=_p0.add(_vTo.scale(_distance));
+        Point3D Pc= _P0.add(_vTo.scale(_distance));   //PC= P0 +d*vTo ->> Calculation from the presentation
+        double Rx= _width/nX;  //Rx= w/nx ->> Calculation from the presentation
+        double Ry=  _height/nY; //Ry= h/ny ->> Calculation from the presentation
 
-        double Rx=_width/nX;
-        double Ry=_height/nY;
+        Point3D pIJ= Pc;
+        double Xj= (j - (nX-1)/2d) * Rx ; //Xj= (j - (nX-1)/2d) * Rx ->> Calculation from the presentation
+        double Yi= -(i- (nY-1)/2d)* Ry; //Yi= -(i- (nY-1)/2d)* Ry;  ->> Calculation from the presentation
 
-        Point3D pIJ=Pc;
-
-        double xj=(j-((nX-1)/2d))*Rx;
-        double yi=-(i-((nY-1)/2d))*Ry;
-        if(!isZero(xj)) {
-            pIJ=pIJ.add(_vRight.scale(xj));
+        if(!isZero(Xj)){
+            pIJ = pIJ.add(_vRight.scale(Xj)); //pIJ= pIJ + _vRight*Xj ->> Calculation from the presentation
         }
-        if(!isZero(yi)) {
-            pIJ=pIJ.add(_vUp.scale(yi));
+        if(!isZero(Yi)){
+            pIJ = pIJ.add(_vUp.scale(Yi)); //pIJ= pIJ + _vUp*Yi ->> Calculation from the presentation
         }
-        Vector Vij=pIJ.subtract(_p0);
-        return new Ray(_p0, Vij);
+
+        Vector Vij= pIJ.subtract(_P0); //Vij= pIJ-P0
+        return new Ray(_P0, Vij);
+
     }
+
     public static class Builder {
-
-        /**constructor*/
-        public Builder(Point3D p0, Vector vTo, Vector vUp) {
-         _p0 = p0;
-         _vTo = vTo.normalized();
-         _vUp = vUp.normalized();
-
-         //if their dot-product is not 0, then they r not orthogonal
-         if (!isZero(_vTo.dotProduct(_vUp))) {
-         throw new IllegalArgumentException("vTo and vUp are not orthogonal");
-         }
-
-         //else
-         _vRight = _vTo.crossProduct(_vUp);
-
-         }
-
-        final Point3D _p0;
-        final Vector _vTo;
-        final Vector _vUp;
-        final Vector _vRight;
+        final private Point3D _P0;  //The eye of the camera
+        final private Vector _vTo;
+        final private Vector _vUp;
+        final private Vector _vRight;
+        //View Plane parameters
+        private double  _width=1;
         private double _height=1;
-        private double _width=1;
-        private double _distance=1;
+        private double _distance=1;  //The distance between the View Plane to the camera
 
+        /**
+         * set the length and width of ViewPlane
+         * @param width  width of the ViewPlane
+         * @return this
+         */
+        public Builder setWidth(double width) {
+            _width = width;
+            return this;
+        }
+        /**
+         * set the length and width of ViewPlane
+         * @param height height of the ViewPlane
+         * @return this
+         */
+        public Builder setHeight(double height) {
+            _height = height;
+            return this;
+        }
+        /**
+         * set the distance of ViewPlane
+         * @param distance The distance between the View Plane to the camera
+         * @return this
+         */
+        public Builder setDistance(double distance) {
+            _distance = distance;
+            return this;
+        }
+
+        public Camera build(){
+            Camera camera= new Camera(this);
+            return camera;
+        }
+
+        public Builder(Point3D p0, Vector vTo, Vector vUp) {
+            _P0 = p0;
+            _vTo = vTo.normalized();
+            _vUp = vUp.normalized();
+            if (!isZero(_vTo.dotProduct(_vUp))) {
+                throw new IllegalArgumentException("vTo and vUp are not orthogonal");
+            }
+            _vRight = _vTo.crossProduct(_vUp); //This vector has been normalized
+        }
     }
-
 }
 
--- tests/elements/CameraIntegrationsTest.java
+++ tests/elements/CameraIntegrationsTest.java
@@ -1,39 +1,199 @@
 package elements;
 
+
+import geometries.Plane;
 import geometries.Sphere;
+import geometries.Triangle;
 import org.junit.jupiter.api.Test;
 import primitives.Point3D;
-import primitives.Ray;
 import primitives.Vector;
 
-import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * integration tests Class
+ *
+ * @author Ruth & Odelia
+ *
+ */
+/*integration tests of creating rays and finding intersections between them and geometries*/
 public class CameraIntegrationsTest {
-    @Test
-    void testSphere1withCam() {
-        Sphere s1=new Sphere(new Point3D(0, 0, -3), 1);
 
-        Camera cam=new Camera(Point3D.ZERO, new  Vector(0, 0, -1), new Vector(0, 1, 0)).setDistance(1).setViewPlaneSize(3, 3);
+    //*creating 2 cameras *//
+    Camera camera0 = new Camera.Builder(Point3D.zero, new Vector(0, 0, 1),
+            new Vector(0, -1, 0))
+            .setDistance(1)
+            .setWidth(3)
+            .setHeight(3)
+            .build();
 
-            List<Point3D> allPoints = null;
+    Camera camera1 = new Camera.Builder(new Point3D(0, 0, -0.5),
+            new Vector(0, 0, 1),
+            new Vector(0, -1, 0))
+            .setDistance(1)
+            .setWidth(3)
+            .setHeight(3)
+            .build();
 
-        public void cameraIntersections(Camera cam) {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    Ray r = cam.constructRayThroughPixel(3, 3, j, i);
-                    List<Point3D> lst = s1.findIntersections(r);
-                    if (lst != null) {
-                        if (allPoints == null) {
-                            allPoints = new LinkedList<>();
-                        }
-                        allPoints.addAll(lst);
-                    }
-                }
+    /**A private help function to prevent repeating the test calculation code for sphere
+     *
+     *
+     * @param Nx       width - Number of pixels on x axis
+     * @param Ny       height - Number of pixels on y axis
+     * @param sphere   The Sphere
+     * @param expected What assertEquals expects to receive
+     * @param message  Printed if the return value is not as expected
+     * @param cam      The camera
+     */
+    public void sphereWithCam(int Nx, int Ny, Sphere sphere, int expected, String message, Camera cam) {
+        List<Point3D> results;
+        int count = 0;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                results = sphere.findIntersections(cam.constructRayThroughPixel(Nx, Ny, j, i));
+                if (results != null)
+                    count += results.size();
             }
         }
-        assertEquals(allPoints.size(), 2, "wrong number of intersections");
+        assertEquals(expected, count, "The result is not as expected");
+    }
+
+    /**
+     * A private help function to prevent repeating the test calculation code for plane
+     *
+     * @param Nx       number of pixels on x axis
+     * @param Ny       number of pixels on y axis
+     * @param plane    The Plane
+     * @param expected What assertEquals expects to receive
+     * @param message  Printed if the return value is not as expected
+     * @param cam      The camera
+     */
+
+    public void planeWithCam(int Nx, int Ny, Plane plane, int expected, String message, Camera cam) {
+        List<Point3D> results;
+        int count = 0;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                results = plane.findIntersections(cam.constructRayThroughPixel(Nx, Ny, j, i));
+                if (results != null)
+                    count += results.size();
+            }
+        }
+        assertEquals(expected, count, "The result is not as expected");
+    }
+
+    /**
+     * A private help function to prevent repeating the test calculation code for triangle
+     *
+     * @param Nx       number of pixels on x axis
+     * @param Ny       number of pixels on y axis
+     * @param triangle The Triangle
+     * @param expected What assertEquals expects to receive
+     * @param message  If not return what we expected to print the this message
+     * @param cam      The camera
+     */
+
+    public void triangleWithCam(int Nx, int Ny, Triangle triangle, int expected, String message, Camera cam) {
+        List<Point3D> results;
+        int count = 0;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                results = triangle.findIntersections(cam.constructRayThroughPixel(Nx, Ny, j, i));
+                if (results != null)
+                    count += results.size();
+            }
+        }
+        assertEquals(expected, count, "The result is not as expected");
+    }
+
+    /*********************************
+     * test intersections Ray/Sphere *
+     ********************************/
+
+    //Sphere first test
+    @Test
+    void constructRayThroughPixelWithSphere1() {
+        Sphere sph = new Sphere(new Point3D(0, 0, 3), 1);
+        sphereWithCam(3, 3, sph, 2, "The result is not as expected", camera0);
+    }
+
+    //Sphere second test
+    @Test
+    void constructRayThroughPixelWithSphere2() {
+        Sphere sph = new Sphere(new Point3D(0, 0, 2.5), 2.5);
+        sphereWithCam(3, 3, sph, 18, "The result is not as expected", camera1);
+    }
+
+    //Sphere third test
+    @Test
+    void constructRayThroughPixelWithSphere3() {
+        Sphere sph = new Sphere(new Point3D(0, 0, 2), 2);
+        sphereWithCam(3, 3, sph, 10, "The result is not as expected", camera1);
+
+    }
+
+    //Sphere fourth test
+    @Test
+    void constructRayThroughPixelWithSphere4() {
+        Sphere sph = new Sphere(new Point3D(0, 0, 1), 4);
+        sphereWithCam(3, 3, sph, 9, "The result is not as expected", camera1);
+    }
+
+    //Sphere fifth test
+    @Test
+    void constructRayThroughPixelWithSphere5() {
+        Sphere sph = new Sphere(new Point3D(0, 0, -1), 0.5);
+        sphereWithCam(3, 3, sph, 0, "The result is not as expected", camera1);
+    }
+
+    /*********************************
+     * test intersections Ray/Plane *
+     ********************************/
+
+    //Plane first test
+    @Test
+    void constructRayThroughPixelWithPlane1() {
+        geometries.Plane plane = new Plane(new Point3D(0, 0, 3), new Vector(0, 0, 1));
+        planeWithCam(3, 3, plane, 9, "The result is not as expected", camera0);
+    }
+
+    //Plane second test
+    @Test
+    void constructRayThroughPixelWithPlane2() {
+        Plane plane = new Plane(new Point3D(0, 0, 2), new Vector(0, -0.5, 1));
+        planeWithCam(3, 3, plane, 9, "The result is not as expected", camera0);
+    }
+
+    //Plane third test
+    @Test
+    void constructRayThroughPixelWithPlane3() {
+        Plane plane = new Plane(new Point3D(0, 0, 2), new Vector(0, -1, 1));
+        planeWithCam(3, 3, plane, 6, "The result is not as expected", camera0);
+    }
+
+    /***********************************
+     * test intersections Ray/Triangle *
+     ***********************************/
+
+    //Triangle first test
+    @Test
+    void constructRayThroughPixelWithTriangle1() {
+        geometries.Triangle triangle = new Triangle(new Point3D(0, -1, 2),
+                new Point3D(1, 1, 2),
+                new Point3D(-1, 1, 2));
+        triangleWithCam(3, 3, triangle, 1, "The result is not as expected", camera0);
+    }
+
+    //Triangle second test
+    @Test
+    void constructRayThroughPixelWithTriangle2() {
+        Triangle triangle = new Triangle(new Point3D(0, -20, 2),
+                new Point3D(1, 1, 2),
+                new Point3D(-1, 1, 2));
+        triangleWithCam(3, 3, triangle, 2, "The result is not as expected", camera0);
     }
 }
+
+
